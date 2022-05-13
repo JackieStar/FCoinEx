f0ef10624af25bdf84f3673d42344270fa7df389
陈耀龙 <chenyaolong@migu.cn>
2022-05-13 16:33:08 +0800

    task#需求更新

---
+++ pages/wallet/recharge-usdt.vue
@@ -0,0 +1,278 @@
+<template>
+	<view class="container">
+		<view class="market-header">
+			<u-icon class="arrow-left" @click="openPage(0)" name="arrow-left" color="#ffffff" size="44" />
+			<view class="market-text">{{ i18n.recharge.title }}</view>
+			<image @click="openPage(1)" src="../../static/images/wallet/list.png" class="right-icon" />
+		</view>
+		<view class="user-info">
+			<image :src="rechargeInfo.coin_icon" class="user-avatar" />
+			<view class="info">
+				<text>{{ rechargeInfo.coin }}</text>
+				<view>{{ i18n.withdraw.network }}：{{ rechargeInfo.coin_type }}</view>
+			</view>
+		</view>
+		<image class="user-bg" src="../../static/images/wallet/bg.png" />
+		<view class="code-wrapper">
+			<img :src="rechargeInfo.recharge_qr" class="code-img" />
+			<!-- #ifdef H5 -->
+			<view class="save-code" @click="saveImg(rechargeInfo.recharge_qr)">{{ i18n.recharge.saveImg }}</view>
+			<!-- #endif -->
+			<!-- #ifdef APP-PLUS -->
+			<view class="save-code" @click="saveImg(rechargeInfo.recharge_qr)">{{ i18n.recharge.saveCode }}</view>
+			<!-- #endif -->
+		</view>
+		<view class="title">
+			<text>{{ i18n.recharge.rechargeAddr }}</text>
+			<u-image class="title-bg" src="../../static/images/wallet/title-long-bg.png" width="144upx" height="12upx" mode="" />
+		</view>
+		<view class="input-wrapper">
+			<text class="address-input">{{ rechargeInfo.recharge_addr }}</text>
+			<view class="copy-btn" @click="handleCopy">{{ i18n.recharge.copyBtn }}</view>
+		</view>
+		<view class="title">
+			<text>{{ i18n.recharge.amount }}</text>
+			<u-image class="title-bg" src="../../static/images/wallet/title-bg.png" width="144upx" height="12upx" mode="" />
+		</view>
+		<view class="money-wrapper">
+			<input type="number" v-model="amount" class="money-input" placeholder-style="color: #454D73;font-size: 26upx;" :placeholder="i18n.recharge.placeholder" />
+		</view>
+		<view><c-tips v-for="(item, index) in rechargeInfo.tips" :text="item" :key="index" /></view>
+		<view class="confirm-btn" @click="openPage(3)">{{ i18n.recharge.submitBtn }}</view>
+	</view>
+</template>
+<script>
+import { mapState, mapActions } from 'vuex';
+import tkiQrcode from '@/components/tki-qrcode/tki-qrcode.vue';
+import { uniIcons } from '@dcloudio/uni-ui';
+import { commonMixin } from '@/common/mixin/mixin.js';
+import CTips from '@/components/c-tips/c-tips.vue';
+export default {
+	components: { tkiQrcode, uniIcons, CTips },
+	mixins: [commonMixin],
+	data() {
+		return {
+			rechargeInfo: {},
+			amount: null
+		};
+	},
+	onShow() {
+		uni.setNavigationBarTitle({
+			title: this.i18n.recharge.title
+		});
+		this.loadData();
+	},
+	computed: {
+		...mapState('user', ['loginInfo'])
+	},
+	methods: {
+		...mapActions('wallet', ['getFinaceInfo']),
+		//请求数据
+		async loadData() {
+			this.getFinaceInfo({ config: 'recharge' }).then(res => {
+				this.rechargeInfo = res.data;
+			});
+		},
+		handleCopy() {
+			let $this = this;
+			uni.setClipboardData({
+				data: this.rechargeInfo.recharge_addr,
+				success: function() {
+					$this.$api.msg($this.i18n.toast.copySuccess);
+				}
+			});
+		},
+		openPage(type) {
+			console.log('type', type);
+			if (type === 0) uni.navigateBack();
+			if (type === 1) {
+				uni.navigateTo({
+					url: '/pages/wallet/record'
+				});
+			}
+			if (type === 3) {
+				if (!this.amount) return this.$api.msg(this.i18n.recharge.placeholder);
+				uni.navigateTo({
+					url: '/pages/wallet/submitRecharge?amount=' + this.amount
+				});
+			}
+		},
+		// 保存图片
+		async saveImg(url) {
+			// // #ifdef H5
+			// this.$api.msg(this.i18n.recharge.saveImg);
+			// // #endif
+			// #ifndef APP-NVUE
+			uni.saveImageToPhotosAlbum({
+				filePath: url,
+				success: () => {
+					this.$api.msg(this.i18n.recharge.saveSuccess);
+				}
+			});
+			// #endif
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+	padding: 40upx 0 100upx 0;
+	.market-header {
+		width: 100%;
+		height: 120upx;
+		text-align: center;
+		left: 0;
+		top: 0;
+		right: 0;
+		background-color: #070219;
+		padding: 0 41upx;
+		position: fixed;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		z-index: 1000;
+		.market-text {
+			height: 120upx;
+			font-size: 36upx;
+			font-family: PingFang SC;
+			font-weight: 400;
+			color: #ffffff;
+			line-height: 120upx;
+		}
+		.right-icon {
+			width: 35upx;
+			height: 40upx;
+		}
+	}
+	.user-info {
+		width: 100%;
+		display: flex;
+		margin-top: 83upx;
+		padding-left: 50upx;
+		.user-avatar {
+			width: 100upx;
+			height: 100upx;
+			margin-right: 38upx;
+			// border-radius: 50%;
+		}
+		.info {
+			display: flex;
+			flex-direction: column;
+			text {
+				font-size: 30upx;
+				font-family: PingFang SC;
+				font-weight: 500;
+				color: #ffffff;
+				margin-top: 14upx;
+			}
+			view {
+				font-size: 24upx;
+				font-family: PingFang SC;
+				font-weight: 400;
+				color: #85a0b2;
+				margin-top: 5upx;
+			}
+		}
+	}
+	.user-bg {
+		width: 750upx;
+		height: 171upx;
+		margin-top: -50upx;
+	}
+	.code-wrapper {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		margin-bottom: 20upx;
+		.code-img {
+			width: 280upx;
+			height: 280upx;
+			margin-top: 30upx;
+		}
+		.save-code {
+			font-size: 26upx;
+			font-family: PingFang SC;
+			font-weight: 400;
+			color: #c2d6e4;
+			margin-top: 30upx;
+		}
+	}
+	.title {
+		width: 100%;
+		height: 28upx;
+		font-size: 30upx;
+		font-family: PingFang SC;
+		font-weight: 500;
+		color: #ffffff;
+		padding: 0 26upx;
+		display: flex;
+		flex-direction: column;
+		text {
+			margin-left: 6upx;
+			z-index: 10;
+		}
+		.title-bg {
+			margin-top: -30upx;
+		}
+	}
+	.input-wrapper {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding: 0 29upx;
+		margin: 40upx 0;
+		.address-input {
+			width: 480upx;
+			font-size: 32upx;
+			font-family: PingFang SC;
+			font-weight: 400;
+			text-decoration: underline;
+			color: #fff;
+			margin-left: 20upx;
+			word-break: break-all;
+		}
+		.copy-btn {
+			width: 154upx;
+			height: 70upx;
+			line-height: 70upx;
+			background: url(../../static/images/wallet/copy-btn.png);
+			background-size: 100% 100%;
+			color: #fff;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			font-size: 26upx;
+		}
+	}
+	.money-wrapper {
+		width: 100%;
+		padding: 0 29upx;
+		margin: 40upx 0;
+		.money-input {
+			width: 690upx;
+			padding: 0 30upx;
+			height: 76upx;
+			border: 2upx solid #554472;
+			border-radius: 4upx;
+			color: #fff;
+		}
+	}
+	.confirm-btn {
+		width: 610upx;
+		height: 76upx;
+		line-height: 76upx;
+		margin: 50upx auto;
+		background: url(../../static/images/public/login-btn.png);
+		background-size: 100% 100%;
+		color: #fff;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 26upx;
+	}
+}
+</style>
--- pages/wallet/withdraw.vue
+++ pages/wallet/withdraw.vue
@@ -1,82 +1,47 @@
 <template>
 	<view class="container">
-		<view class="market-header">
-			<u-icon class="arrow-left" @click="openPage(0)" name="arrow-left" color="#ffffff" size="44" />
-			<view class="market-text">{{ i18n.withdraw.title }}</view>
-			<image @click="openPage(1)" src="../../static/images/wallet/list.png" class="right-icon" />
-		</view>
-		<view class="user-info">
-			<image :src="withdrawInfo.coin_icon" class="user-avatar" />
-			<view class="info">
-				<text>{{ withdrawInfo.coin }}</text>
-				<view>{{ i18n.withdraw.network }}：{{ withdrawInfo.coin_type }}</view>
-			</view>
-		</view>
-		<image class="user-bg" src="../../static/images/wallet/bg.png" />
-		<view class="title">
-			<text>{{ i18n.withdraw.withdrawwAddr }}</text>
-			<u-image class="title-bg" src="../../static/images/wallet/title-bg.png" width="74upx" height="12upx" mode="" />
-		</view>
-		<view class="input-wrapper">
-			<input type="text" placeholder-style="color: #818FA; font-size: 26upx" :placeholder="i18n.withdraw.inputAddr" v-model="withdraw_addr" class="address-input" />
-			<view class="copy-btn" @click="handlePaste">{{ i18n.withdraw.copyBtn }}</view>
-		</view>
-		<view class="title">
-			<text>{{ i18n.withdraw.money }}</text>
-			<u-image class="title-bg" src="../../static/images/wallet/title-bg.png" width="74upx" height="12upx" mode="" />
-		</view>
-		<view class="money-wrapper">
-			<input type="number" v-model="amount" @input="inputChange" placeholder-style="color: #818FAB; font-size: 26upx" class="money-input" placeholder="0.00" />
-			<view class="all-btn" @click="handleAll">{{ i18n.withdraw.all }}</view>
-			<text>USTD</text>
-		</view>
-		<view class="tips-wrapper">
-			<text>{{ i18n.withdraw.balance }}：{{ userData.balance }}</text>
-			<text style="color:#fff">{{ i18n.withdraw.fee }}：{{ withdraw_fee || withdrawInfo.withdraw_min_fee }}</text>
-		</view>
-		<view><c-tips v-for="(item, index) in withdrawInfo.tips" :text="item" :key="index" /></view>
-		<view class="confirm-btn" @click="openModal">{{ i18n.withdraw.btn }}</view>
-		<u-popup v-model="showModal" mode="bottom" class="password-modal" :border-radius="20" height="565upx">
-			<view class="title" style="margin-top: 40upx;">
-				<text>{{ i18n.withdraw.safe }}</text>
-				<u-image class="title-bg" src="../../static/images/wallet/title-long-bg.png" width="144upx" height="12upx" />
-			</view>
-			<view class="modal-line-title">{{ i18n.withdraw.emailCode }}</view>
-			<view class="modal-input-wrapper">
-				<input
-					class="input-item"
-					placeholder-style="color: #818FA; font-size: 26upx"
-					v-model="form.email_code"
-					:placeholder="i18n.withdraw.placeholder"
-					maxlength="10"
-					style="padding-right: 150upx;"
-					type="text"
-				/>
-				<view>
-					<u-verification-code
-						:start-text="i18n.common.getCode"
-						:change-text="i18n.common.seconds"
-						:end-text="i18n.common.again"
-						:seconds="seconds"
-						ref="uCode"
-						@change="codeChange"
-					></u-verification-code>
-					<view @tap="getCode" class="code-btn">{{ tips }}</view>
+		<!-- 充值提现 -->
+		<view class="menu">
+			<view class="fiat m-r" @click="navTo('/pages/wallet/withdraw-pix')">
+				<image class="menu-icon" src="../../static/images/makets/recharge.png" mode="widthFix" />
+				<view class="label">
+					<!-- <text>{{ i18n.wallet.recharge }}</text> -->
+					<text>PIX提现</text>
 				</view>
 			</view>
-			<view class="modal-line-title">{{ i18n.withdraw.loginPwd }}</view>
-			<view class="modal-input-wrapper">
-				<input
-					class="input-item"
-					placeholder-style="color: #818FA; font-size: 26upx"
-					v-model="form.password"
-					:placeholder="i18n.withdraw.placeholder1"
-					maxlength="10"
-					type="password"
-				/>
+			<view class="fiat m-l" @click="navTo('/pages/wallet/withdraw-usdt')">
+				<image class="menu-icon" src="../../static/images/makets/withdraw.png" mode="widthFix" />
+				<view class="label">
+					<!-- <text>{{ i18n.wallet.withdraw }}</text> -->
+					<text>USDT提现</text>
+				</view>
 			</view>
-			<view class="submit-btn" @click="handleSubmit">{{ i18n.withdraw.submitBtn }}</view>
-		</u-popup>
+		</view>
+		<!-- 交易流水 -->
+		<view class="trade-title">提现记录</view>
+		<view class="tabs-wrapper">
+			<view class="tabs-item" @click="handleChange(1)">
+				<u-image v-if="tabIndex === 1" class="title-bg" src="../../static/images/wallet/title-long-bg.png" width="144upx" height="12upx" mode="" />
+				<text :style="{ color: tabIndex === 1 ? '#fff' : '#646F7B', 'margin-top': tabIndex === 1 ? '' : '-10upx' }">{{ i18n.record.rechargeList }}</text>
+			</view>
+			<view class="tabs-item" @click="handleChange(2)">
+				<u-image v-if="tabIndex === 2" class="title-bg" src="../../static/images/wallet/title-long-bg.png" width="144upx" height="12upx" mode="" />
+				<text :style="{ color: tabIndex === 2 ? '#fff' : '#646F7B', 'margin-top': tabIndex === 2 ? '' : '-10upx' }">{{ i18n.record.withdrawList }}</text>
+			</view>
+		</view>
+		<view class="trade-list-wrapper">
+			<view class="trade-list" v-for="(item, index) in tradeList" :key="index">
+				<view class="trade-money">
+					<text>{{ item.description }}</text>
+					<text v-if="item.atype == 'out'">- {{ item.amount }}</text>
+					<text v-else>{{ item.amount }}</text>
+				</view>
+				<view class="trade-time">
+					<text>{{ item.cdate }}</text>
+					<text>USDT</text>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -84,59 +49,65 @@
 import { mapState, mapActions } from 'vuex';
 import { uniIcons } from '@dcloudio/uni-ui';
 import { commonMixin } from '@/common/mixin/mixin.js';
-// 函数防抖 (只执行最后一次点击)
-const debounce = (fn, t) => {
-	let delay = t || 500;
-	let timer;
-	return function() {
-		let args = arguments;
-		if (timer) {
-			clearTimeout(timer);
-		}
-		timer = setTimeout(() => {
-			timer = null;
-			fn.apply(this, args);
-		}, delay);
-	};
-};
 export default {
 	components: { uniIcons },
 	mixins: [commonMixin],
 	data() {
-		this.inputChange = debounce(this.inputChange, 800);
 		return {
-			withdrawInfo: {},
+			tabIndex: 1,
+			bgColor: '#070219',
 			userData: {},
-			coin_type: null, // 网络
-			amount: null, // 提现金额
-			withdraw_addr: null, // 提现地址
-			showModal: false,
-			seconds: 60,
-			tips: '',
-			form: {
-				email_code: null,
-				password: null
-			},
-			withdraw_fee: null // 提现手续费，计算
+			tradeList: [],
+			page: 1,
+			isHavePage: true,
+			isSendLoading: false
 		};
 	},
-
 	onShow() {
 		uni.setNavigationBarTitle({
-			title: this.i18n.withdraw.title
+			title: this.i18n.wallet.title
 		});
-		this.loadData();
-		this.getUserInfo();
+		if (this.loginInfo.hasLogin) {
+			this.getUserInfo();
+			this.loadData();
+		} else {
+			uni.navigateTo({
+				url: '/pages/public/login'
+			});
+		}
+	},
+	computed: {
+		...mapState('user', ['loginInfo'])
 	},
 	methods: {
-		...mapActions('common', ['sendSms']),
-		...mapActions('wallet', ['getFinaceInfo', 'withdraw', 'withdrawFee']),
 		...mapActions('user', ['userInfo']),
+		...mapActions('wallet', ['getAccountLogs']),
 		//请求数据
-		async loadData() {
-			this.getFinaceInfo({ config: 'withdraw' }).then(res => {
-				this.withdrawInfo = res.data;
-			});
+		loadData() {
+			this.isSendLoading = true;
+			let parmas = {
+				page: this.page,
+				limit: 10
+			};
+			if (!this.isHavePage) return this.$api.msg(this.i18n.toast.noMore);
+			this.getAccountLogs(parmas)
+				.then(res => {
+					this.isSendLoading = false;
+					if (this.page == 1) {
+						this.tradeList = res.data.data;
+					} else {
+						if (res.data.data.length >= 10) {
+							this.isHavePage = true;
+						} else {
+							this.isHavePage = false;
+						}
+						this.tradeList = this.tradeList.concat(res.data.data);
+					}
+				})
+				.catch(error => {});
+		},
+		handleChange(type) {
+			this.tabIndex = type;
 		},
 		// 获取用户信息
 		getUserInfo() {
@@ -144,330 +115,141 @@ export default {
 				this.userData = res.data;
 			});
 		},
-		// 全部
-		handleAll() {
-			this.amount = this.userData.balance;
-		},
-		// 交易密码弹窗
-		openModal() {
-			if (!this.withdraw_addr) {
-				this.$api.msg(this.i18n.withdraw.noWithdrawAddr);
-				return;
+		onReachBottom() {
+			console.log('3333', this.isSendLoading);
+			if (!this.isSendLoading) {
+				this.page++;
+				this.loadData();
 			}
-			if (!this.amount) {
-				this.$api.msg(this.i18n.withdraw.noAmount);
-				return;
-			}
-			this.form = {
-				email_code: null,
-				password: null
-			};
-			this.showModal = true;
-		},
-		handleSubmit() {
-			if (!this.form.email_code) {
-				this.$api.msg(this.i18n.withdraw.noEmailCode);
-				return;
-			}
-			if (!this.form.password) {
-				this.$api.msg(this.i18n.withdraw.noPwd);
-				return;
-			}
-			let params = {
-				coin_type: this.withdrawInfo.coin_type,
-				withdraw_addr: this.withdraw_addr,
-				amount: this.amount,
-				...this.form
-			};
-			this.withdraw(params).then(res => {
-				this.$api.msg(this.i18n.withdraw.withdrawSuccess);
-				this.showModal = false;
-			});
-		},
-		codeChange(text) {
-			this.tips = text;
-		},
-		getCode() {
-			if (this.$refs.uCode.canGetCode) {
-				uni.showLoading({
-					title: this.i18n.toast.coding
-				});
-				let data = {
-					usage: 'withdraw',
-					email: this.loginInfo.email
-				};
-				this.sendSms(data)
-					.then(res => {
-						uni.hideLoading();
-						this.$u.toast(this.i18n.toast.codeSend);
-						// 通知验证码组件内部开始倒计时
-						this.$refs.uCode.start();
-					})
-					.catch(error => {});
-			}
-		},
-		// 粘贴
-		handlePaste() {
-			uni.getClipboardData({
-				success: res => {
-					this.withdraw_addr = res.data;
-					console.log(res.data);
-				}
-			});
-		},
-		openPage(type) {
-			if (type === 0) uni.navigateBack();
-			if (type === 1)
-				uni.navigateTo({
-					url: '/pages/wallet/record'
-				});
-		},
-		inputChange(e) {
-			console.log('e', e.detail.value);
-
-			this.withdrawFee({ amount: e.detail.value }).then(res => {
-				this.withdraw_fee = res.data.fee;
-				console.log('e2222', this.withdraw_fee);
-			});
 		}
 	}
 };
 </script>
 
 <style lang="scss" scoped>
+page {
+	background: #070219;
+}
 .container {
-	padding-top: 100upx;
-	.market-header {
-		width: 100%;
+	padding: 40upx 26upx;
+}
+.title-wrapper {
+	width: 100%;
+	// height: 28upx;
+	font-size: 30upx;
+	font-family: PingFang SC;
+	font-weight: 500;
+	color: #ffffff;
+	padding: 30upx 0;
+	display: flex;
+	flex-direction: column;
+	box-sizing: border-box;
+	text {
+		margin-left: 6upx;
+		z-index: 10;
+	}
+	.title-bg {
+		margin-top: -30upx;
+	}
+}
+.tabs-wrapper {
+	width: 100%;
+	height: 140upx;
+	display: flex;
+	align-items: center;
+	.tabs-item {
+		width: 50%;
 		height: 140upx;
-		text-align: center;
-		left: 0;
-		top: 0;
-		right: 0;
-		background-color: #070219;
-		padding: 0 41upx;
-		position: fixed;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		.market-text {
-			height: 140upx;
-			font-size: 36upx;
-			font-family: PingFang SC;
-			font-weight: 400;
-			color: #ffffff;
-			line-height: 140upx;
-		}
-		.right-icon {
-			width: 35upx;
-			height: 40upx;
-		}
-	}
-	.user-info {
-		width: 100%;
-		display: flex;
-		margin-top: 83upx;
-		padding-left: 50upx;
-		.user-avatar {
-			width: 112upx;
-			height: 112upx;
-			margin-right: 38upx;
-		}
-		.info {
-			display: flex;
-			flex-direction: column;
-			text {
-				font-size: 30upx;
-				font-family: PingFang SC;
-				font-weight: 500;
-				color: #ffffff;
-				margin-top: 14upx;
-			}
-			view {
-				font-size: 24upx;
-				font-family: PingFang SC;
-				font-weight: 400;
-				color: #85a0b2;
-				margin-top: 5upx;
-			}
-		}
-	}
-	.user-bg {
-		width: 750upx;
-		height: 171upx;
-		margin-top: -50upx;
-	}
-	.title {
-		width: 100%;
-		height: 28upx;
 		font-size: 30upx;
 		font-family: PingFang SC;
 		font-weight: 500;
-		color: #ffffff;
-		padding: 0 26upx;
 		display: flex;
 		flex-direction: column;
+		align-items: center;
+		margin-top: 100upx;
 		text {
-			margin-left: 6upx;
+			margin-left: 2upx;
 			z-index: 10;
 		}
 		.title-bg {
-			margin-top: -30upx;
+			margin-bottom: -22upx;
 		}
 	}
-	.input-wrapper {
-		width: 100%;
+}
+.menu {
+	// padding: 20upx 24upx;
+	margin-top: 47upx;
+	display: flex;
+	justify-content: space-between;
+	font-size: $font-base;
+	color: $border-color-light;
+	font-weight: bold;
+	.fiat {
+		width: 330rpx;
+		height: 110rpx;
 		display: flex;
+		flex: 1;
 		align-items: center;
+		background: #1a1b28;
+		border-radius: 10rpx;
+		padding: 0 45upx;
 		justify-content: space-between;
-		padding: 0 29upx;
-		margin: 40upx 0;
-		.address-input {
-			width: 511upx;
-			height: 76upx;
-			border: 2upx solid #554472;
-			border-radius: 4upx;
-			color: #fff;
-			padding-left: 24upx;
-		}
-		.copy-btn {
-			width: 153upx;
-			height: 76upx;
-			line-height: 76upx;
-			background: url(../../static/images/public/short-btn.png);
-			background-size: 100% 100%;
-			color: #fff;
+		.label {
 			display: flex;
-			align-items: center;
-			justify-content: center;
-			font-size: 26upx;
+			flex-direction: column;
+			padding-left: 20upx;
 		}
-	}
-	.money-wrapper {
-		width: 100%;
-		padding: 0 29upx;
-		margin-top: 40upx;
-		.money-input {
-			width: 690upx;
-			height: 76upx;
-			border: 2upx solid #554472;
-			border-radius: 4upx;
-			padding: 0 20upx;
-			padding-right: 300upx;
-			color: #fff;
+		.sub {
+			font-size: $font-sm;
+			font-weight: normal;
+		}
+		.menu-icon {
+			width: 44rpx;
 		}
 		text {
-			margin-right: 190upx;
-			float: right;
-			font-size: 28upx;
-			font-family: PingFang SC;
-			font-weight: 400;
-			color: #63798f;
-			opacity: 0.6;
-			margin-top: -58upx;
+			font-size: $font-md;
 		}
-		.all-btn {
-			width: 160upx;
-			height: 60upx;
-			line-height: 60upx;
-			border-left: 2upx solid #554472;
-			text-align: center;
-			margin-right: 5upx;
-			float: right;
-			font-size: 32upx;
+	}
+	.m-r {
+		margin-right: 14upx;
+	}
+	.m-l {
+		margin-left: 14upx;
+	}
+}
+.trade-title {
+	font-size: 24upx;
+	font-family: PingFang SC;
+	font-weight: 400;
+	color: #8a959f;
+	margin: 34upx 0 28upx 0;
+}
+.trade-list {
+	padding: 20upx 40upx;
+	background-color: #1a1a1a;
+	border-bottom: 1upx solid #303030;
+	.trade-money {
+		width: 100%;
+		display: flex;
+		justify-content: space-between;
+		text {
+			font-size: 30upx;
 			font-family: PingFang SC;
-			margin-top: -68upx;
 			font-weight: 400;
 			color: #ffffff;
-			background: linear-gradient(0deg, #3fbbfe 0%, #a541ff 100%);
-			-webkit-background-clip: text;
-			-webkit-text-fill-color: transparent;
 		}
 	}
-	.tips-wrapper {
+	.trade-time {
 		width: 100%;
-		padding: 0 32upx;
 		display: flex;
-		align-items: center;
+		margin-top: 4upx;
 		justify-content: space-between;
-		font-size: 26upx;
-		font-family: PingFang SC;
-		font-weight: 400;
-		color: #818fab;
-		margin-top: 31upx;
-		margin-bottom: 120upx;
-	}
-	.confirm-btn {
-		width: 610upx;
-		height: 76upx;
-		line-height: 76upx;
-		margin: 50upx auto;
-		background: url(../../static/images/public/login-btn.png);
-		background-size: 100% 100%;
-		color: #fff;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		font-size: 26upx;
-	}
-	.password-modal {
-		width: 694upx;
-		margin: 0 auto;
-		border-radius: 20upx;
-		margin-bottom: 60upx;
-		::v-deep .u-drawer-bottom {
-			background-color: #262c4a;
-		}
-	}
-	.modal-line-title {
-		font-size: 24upx;
-		font-family: PingFang SC;
-		font-weight: 400;
-		color: #ffffff;
-		padding-left: 30upx;
-		margin-top: 40upx;
-		margin-bottom: 20upx;
-	}
-	.modal-input-wrapper {
-		width: 638upx;
-		height: 74upx;
-		background: url(../../static/images/public/login-btn.png);
-		background-size: 100% 100%;
-		margin-left: 30upx;
-
-		.input-item {
-			width: 440upx;
-			height: 76upx;
-			padding: 0 32upx;
-			color: #fff;
-		}
-		.code-btn {
-			float: right;
-			margin-top: -56upx;
-			margin-right: 50upx;
-			border-left: 1upx solid #dadada;
-			padding-left: 20upx;
-			color: #4f5b87;
-			font-size: 26upx;
+		text {
+			font-size: 24upx;
 			font-family: PingFang SC;
 			font-weight: 400;
-			background: linear-gradient(0deg, #3fbbfe 0%, #a541ff 100%);
-			-webkit-background-clip: text;
-			-webkit-text-fill-color: transparent;
+			color: #5c6672;
 		}
-	}
-	.submit-btn {
-		width: 638upx;
-		height: 80upx;
-		line-height: 80upx;
-		margin: 46upx auto;
-		color: #fff;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		font-size: 26upx;
-		background: linear-gradient(90deg, #3fbbfe, #a541ff);
-		border-radius: 4upx;
 	}
 }
 </style>
